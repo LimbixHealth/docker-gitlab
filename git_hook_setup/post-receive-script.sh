693c8b7bf36562ae2b91a2fad27fb7bce3805326
Josh Klag <josh@limbix.com>
2019-09-11 13:23:26 -0700

    in post-receive-script, cd to directory instead of passing in args to git commands

--- git_hook_setup/post-receive-script.sh
+++ git_hook_setup/post-receive-script.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
-set -x
 REPO_URL="ssh://git@192.168.86.132:10022/root/limbix-content.git"
 NAS_CONTENT_DIR="/NAS-content"
 
 branch_name="$1"
 repo_dir="${NAS_CONTENT_DIR}/${branch_name}"
 
+# store current path so it can be returned to later
+cur_path=$(pwd)
+
 if [ -d $repo_dir ]
 then
-  git --work-tree="${repo_dir}" --git-dir="${repo_dir}"/.git pull origin "$branch_name"
+  # pull from branch
+  cd ${repo_dir}
+  git pull origin "$branch_name"
 else
+  # clone branch
   git clone -b $branch_name --single-branch $REPO_URL $repo_dir --depth 1
-  git --work-tree="${repo_dir}" --git-dir="${repo_dir}"/.git lfs install
+  cd ${repo_dir}
+  git lfs install
 fi
 
-git --work-tree="${repo_dir}" --git-dir="${repo_dir}"/.git lfs pull origin "$branch_name"
+# pull all files
+git lfs pull origin "$branch_name"
 
+# return to original working directory and remove lock file
+cd ${cur_path}
 rm "${branch_name}.lock"
